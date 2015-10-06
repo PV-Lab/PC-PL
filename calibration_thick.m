2743c1c86e3c6728551f126b9741124bb54fedbd
maljensen <jensenma@mit.edu>
2015-10-06 09:32:35 -0400

    Modifying sample parameters and deleting information unrelated to current measurement

--- calibration_thick.m
+++ calibration_thick.m
@@ -1,12 +1,12 @@
 function [p] = calibration_thick
 
 %PL filenames (Ex. 187_10s_20LP_1.txt)
-sample_no_PL = 'C:\Users\Mallory\Documents\Non-contact crucible\9-15-2015 experiment TR+Amanda\Lifetime stage 1\PC-PL\19-3 5s\19-3'; %this is the first part of the filenames
-sample_no_xls = 'C:\Users\Mallory\Documents\Non-contact crucible\9-15-2015 experiment TR+Amanda\Lifetime stage 1\PC-PL\19-3 5s\19-3';
+sample_no_PL = 'C:\Users\Mallory\Documents\Non-contact crucible\9-15-2015 experiment TR+Amanda\Lifetime stage 1\PC-PL\20-6\20-6'; %this is the first part of the filenames
+sample_no_xls = 'C:\Users\Mallory\Documents\Non-contact crucible\9-15-2015 experiment TR+Amanda\Lifetime stage 1\PC-PL\20-6\20-6';
 exposure = 5; %seconds, this is the second part of the filename
 LP = [22 25 30 35 40 45 50 55 60 65 70 75 80]; %This is the last part of the filename
 filename_after_PL='LP_1.txt';
-sensor = 'C:\Users\Mallory\Documents\Non-contact crucible\9-15-2015 experiment TR+Amanda\Lifetime stage 1\PC-PL\19-3 5s\Sinton_circle_1.txt'; %This is the PL file with the Sinton circle image
+sensor = 'C:\Users\Mallory\Documents\Non-contact crucible\9-15-2015 experiment TR+Amanda\Lifetime stage 1\PC-PL\PCPL_10-2-2015\Sinton_circle_1.txt'; %This is the PL file with the Sinton circle image
 filename_after_PC='LP.xlsm';
 
 %Get matrices with averages of PL/PC signals. These matrices should have
