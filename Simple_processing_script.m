9b98d80266ea5716c32830577f3bb6d7697deabb
maljensen <jensenma@mit.edu>
2015-10-13 12:29:42 -0400

    Modifying input parameters for data processing

--- Simple_processing_script.m
+++ Simple_processing_script.m
@@ -4,12 +4,12 @@ clear all; close all;
 filenameSintonCircle = 'C:\Users\Mallory\Documents\Non-contact crucible\9-15-2015 experiment TR+Amanda\Lifetime stage 1\PC-PL\PCPL_10-2-2015\Sinton_circle_1.txt';
 
 %Get PL data
-load('C:\Users\Mallory\Documents\Non-contact crucible\9-15-2015 experiment TR+Amanda\Lifetime stage 1\PC-PL\15-9-21-N\15-9-21-N_PCPL_CodeFix.mat');
+load('C:\Users\Mallory\Documents\Non-contact crucible\9-15-2015 experiment TR+Amanda\Lifetime stage 1\PC-PL\19-2\19-2_PCPL_CodeFix.mat');
 deltanLifetime = deltan; 
 tauLifetime = tau; 
 
 %Get QSSPC data 
-load('C:\Users\Mallory\Documents\Non-contact crucible\9-15-2015 experiment TR+Amanda\Lifetime stage 1\No box\15-9-21-N\LifetimeData.mat');
+load('C:\Users\Mallory\Documents\Non-contact crucible\9-15-2015 experiment TR+Amanda\Lifetime stage 1\No box\19-2\LifetimeData.mat');
 deltanQSSPC = deltanq; 
 tauQSSPC = tau_mean.*1e6; 
 
