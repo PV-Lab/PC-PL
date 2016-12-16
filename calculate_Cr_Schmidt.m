0d295dc0e503f6fd50cdcb1f3922002576a639e9
maljensen <jensenma@mit.edu>
2016-12-15 22:43:38 -0500

    adding MIT license to each file

--- calculate_Cr_Schmidt.m
+++ calculate_Cr_Schmidt.m
@@ -1,3 +1,27 @@
+%{
+MIT License
+
+Copyright (c) [2016] [Mallory Ann Jensen, jensenma@alum.mit.edu]
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+%}
+
 function [Cr,error,Cr_correct,Cr_zero,Cr_error,store_ltz,store_lte,store_nanerror] = calculate_Cr_Schmidt(N_A,T_assoc,T_dissoc,time,tau_Cri,dtau_Cri, deltan_Cri,tau_CrB,dtau_CrB, deltan_CrB)
 %This function was edited on 10/16/14 to include defect values from Schmidt
 %JPV 2013. 
