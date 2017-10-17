f3c3b4732ce06105c622918f59a8e2710ee2294c
maljensen <jensenma@mit.edu>
2017-10-17 16:22:35 -0400

    first commit of new script used to analyze PL for samples throughout degradation

---
+++ degradation_PL.m
@@ -0,0 +1,162 @@
+%{
+MIT License
+
+Copyright (c) [2016] [Mallory Ann Jensen, jensenma@alum.mit.edu]
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+%}
+
+%Processing PL data - degradation at different points
+%Written by Mallory Jensen
+%October 17, 2017
+%% Get the average injection levels
+clear all; close all; clc; 
+%the PL maps should already be calibrated and saved as .mat files
+%Locations of the different PL maps corresponding to different times
+initial_PL = 'C:\Users\Mallory Jensen\Documents\LeTID\Dartboard\Repassivated samples\PCPL\PCPL August 8 2017'; 
+deg_PL_1 = 'C:\Users\Mallory Jensen\Documents\LeTID\Dartboard\Repassivated samples\PCPL\8000s PL'; 
+times = {'0s' '8000s'}; 
+dirnames = {initial_PL,deg_PL_1};
+
+%Samples analyzed
+samples = {'Mo-L-5','Ti-L-5','Ni-L-5','V-L-5','C-L-5',...
+    'Mo-h-5','Ti-h-5','Ni-h-5','V-h-5','C-h-5'}; 
+
+deltan_avg = cell(size(samples)); 
+%Loop over and do the same thing for each sample
+for i = 1:length(samples)
+    %Loop over and do the same thing for each time measured
+    for j = 1:length(times)
+        %Make the filename
+        filename = [dirnames{j} '\' samples{i} '_calibrated.mat']; 
+        load(filename); 
+        %The variables we have: 'PLmaps','deltan','tau','LP','exp_sample',
+        %'Flux_808','calibration','doping_samp','optical_map'); 
+        %Loop over the laser powers
+        if j == 1
+            deltan_avg_now = zeros(length(times),length(LP));
+        end
+        for k = 1:length(LP)
+            deltan_now = deltan{k}; 
+            %Now crop and get an average of the injection level
+            if i==1 && j == 1 && k == 1
+                [deltan_now,x_crop,y_crop]=crop_PL(deltan_now);
+            else
+                %crop according to the first attempt
+                [m,n] = size(deltan_now);
+                %Get rid of highest y values we don't want
+                deltan_now(floor(y_crop(2)):m,:) = [];
+                %Get rid of highest x values we don't want
+                deltan_now(:,floor(x_crop(2)):n) = [];
+                %Get rid of lowest y values we don't want
+                deltan_now(1:floor(y_crop(1)),:) = [];
+                %Get rid of lowest x values we don't want
+                deltan_now(:,1:floor(x_crop(1))) = [];
+                %Show us the result just for a visual confirmation
+                figure;
+                imagesc(deltan_now);
+                axis('image');
+                colormap(gray);
+            end
+            [linear,M_p] = tau_averages(deltan_now); 
+            deltan_avg_now(j,k) = M_p; 
+        end
+    end
+    deltan_avg{i} = deltan_avg_now; 
+end
+
+%% Compare degradation points
+%Now we need to decide which laser powers to compare throughout degradation based on the averages
+clear all; close all; clc; 
+%the PL maps should already be calibrated and saved as .mat files
+%Locations of the different PL maps corresponding to different times
+initial_PL = 'C:\Users\Mallory Jensen\Documents\LeTID\Dartboard\Repassivated samples\PCPL\PCPL August 8 2017'; 
+deg_PL_1 = 'C:\Users\Mallory Jensen\Documents\LeTID\Dartboard\Repassivated samples\PCPL\8000s PL'; 
+times = {'0s' '8000s'}; 
+dirnames = {initial_PL,deg_PL_1};
+savedirname = 'C:\Users\Mallory Jensen\Documents\LeTID\Dartboard\Repassivated samples\PCPL';
+
+%Samples analyzed
+samples = {'Mo-L-5','Ti-L-5','Ni-L-5','V-L-5','C-L-5',...
+    'Mo-h-5','Ti-h-5','Ni-h-5','V-h-5','C-h-5'}; 
+% samples = {'Mo-L-5'};
+
+%Laser power indices to match
+lps = {[1 3],[2 2],[1 3],[1 2],[1 2],...
+    [1 4],[2 2],[1 4],[1 5],[1 3]};
+% lps = {[1 3]}; 
+
+%Loop over and do the same thing for each sample
+for i = 1:length(samples)
+    deg_plots = figure('units','normalized','outerposition',[0 0 1 1]); 
+    tau_maps = cell(size(times)); 
+    optical_maps = cell(size(times)); 
+    lps_now = lps{i}; 
+    %Loop over and do the same thing for each time measured
+    for j = 1:length(times)
+        %Make the filename
+        filename = [dirnames{j} '\' samples{i} '_calibrated.mat']; 
+        load(filename); 
+        %The variables we have: 'PLmaps','deltan','tau','LP','exp_sample',
+        %'Flux_808','calibration','doping_samp','optical_map'); 
+        %Loop over the laser powers
+        %We need to get the right laser power  for this sample
+        tau_now = tau{lps_now(j)}; 
+        tau_maps{j} = tau_now; 
+        optical_maps{j} = optical_map; 
+        if j == 1
+            figure(deg_plots); 
+            %Then plot the first image
+            subplot(2,length(times),j); 
+            imagesc(tau_now);
+            axis('image');
+            axis off; 
+            colormap('gray');
+            colorbar; 
+            title([times{j} ', LP = ' num2str(LP(lps_now(j)))]); 
+        else
+            %Then we need to align this map to the original map
+            [tau_aligned]=align_maps(tau_maps{1},tau_now);
+            %Plot the new aligned map
+            figure(deg_plots); 
+            subplot(2,length(times),j); 
+            imagesc(tau_aligned);
+            axis('image');
+            axis off; 
+            colormap('gray');
+            colorbar; 
+            title([times{j} ', LP = ' num2str(LP(lps_now(j)))]);
+            %Now also make a difference map relative to the initial
+            diff_map = tau_aligned./tau_maps{1}; 
+            subplot(2,length(times),length(times)+j); 
+            imagesc(diff_map);
+            axis('image');
+            axis off; 
+            caxis([0 1]);
+            colormap('gray');
+            colorbar; 
+            title(['ratio ' times{j}]);  
+        end
+    end
+    %Save the figure
+    set(deg_plots,'defaultAxesFontSize', 20)
+    hgsave(deg_plots,[savedirname '\' samples{i} '_PCPL_summary']);
+    print(deg_plots,'-dpng','-r0',[savedirname '\' samples{i} '_PCPL_summary.png']);
+end
+    
